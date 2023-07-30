4ba9c8041ee2ba574b47c8228e20300eb93149f1
Michal Talaga <mikeon@objectware.pl>
2023-07-31 00:55:05 +0200

    added frontend

---
+++ frontend/src/components/calendar/MiniCalendar.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+import Card from "components/card";
+import "react-calendar/dist/Calendar.css";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import "assets/css/MiniCalendar.css";
+
+const MiniCalendar = () => {
+  const [value, onChange] = useState(new Date());
+  const handleChange = (newValue: Date | Date[], event: React.MouseEvent<HTMLButtonElement>) => {
+    if (Array.isArray(newValue)) {
+      // This is a date range, you can handle it here if needed
+    } else {
+      // This is a single date
+      onChange(newValue);
+    }
+  }
+  return (
+    <div>
+      <Card extra="flex w-full h-full flex-col px-3 py-3">
+        <Calendar
+          onChange={handleChange}
+          value={value}
+          prevLabel={<MdChevronLeft className="ml-1 h-6 w-6 " />}
+          nextLabel={<MdChevronRight className="ml-1 h-6 w-6 " />}
+          view={"month"}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default MiniCalendar;
