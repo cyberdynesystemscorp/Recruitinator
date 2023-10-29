3ff8706769143d93e42c89192f2d470e559f105a
Michal Talaga <mikeon@objectware.pl>
2023-10-29 16:13:12 +0100

    remove debug info from jobs

--- frontend/src/views/admin/jobs/index.tsx
+++ frontend/src/views/admin/jobs/index.tsx
@@ -12,7 +12,6 @@ const JobBoard = () => {
   return (
     <div>
       {/* Card widget */}
-      <h1>{JSON.stringify(user || "undefined")} - is loading {isLoading} - is auth {isAuthenticated}</h1>
       <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6">
         <Widget
           icon={<MdBarChart className="h-7 w-7" />}
